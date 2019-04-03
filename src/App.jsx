deb0b9055eae72a63f1bb9aa1e59066da64a53c8
Jefferson Vieira da Silva <s2it_jvieira@uolinc.com>
2019-04-03 14:58:37 -0300

    SGF-5 Reorganizando o router e corrigindo erros

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import Sidebar from 'components/Sidebar';
-import Header from 'components/Header';
 import Router from 'routes';
 import Toastr from 'components/templates/Toastr';
 import Loading from 'components/Loading';
 
 class App extends Component {
-  state = {
-    showSidebar: true
-  };
-
-  toggleSidebar = () => {
-    this.setState({ showSidebar: !this.state.showSidebar });
-  };
-
   render() {
-    const { loading, user } = this.props;
-    const { showSidebar } = this.state;
+    const { loading } = this.props;
 
     return (
       <div className="wrapper">
-        <Sidebar show={showSidebar} />
-        <div className="content">
-          <Header
-            sidebar={[showSidebar, this.toggleSidebar]}
-            user={user}
-            loggout={() => alert('sair')}
-          />
-          <div className="container-fluid ">
-            <Router />
-          </div>
-        </div>
+        <Router />
         <Toastr />
         <Loading show={loading} />
       </div>
@@ -41,8 +20,7 @@ class App extends Component {
 }
 
 const mapStateToProps = state => ({
-  loading: state.status.loading,
-  user: state.user.user
+  loading: state.status.loading
 });
 
 App = connect(
