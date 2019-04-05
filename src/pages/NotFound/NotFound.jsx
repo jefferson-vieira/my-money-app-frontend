455363a914664cd6caf29640decf419f95ce708b
Jefferson Vieira da Silva <s2it_jvieira@uolinc.com>
2019-04-05 16:40:12 -0300

    SGF-6 Modal de sucesso no cadastro

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link }from 'react-router-dom'
 
 const NotFound = () => (
   <section id="not-found" className="not-found">
@@ -6,6 +7,7 @@ const NotFound = () => (
       <div className="not-found__message my-5">
         <h2>Página não encontrada.</h2>
         <h3>Clique no botão abaixo para ir até a página inicial.</h3>
+        <Link to="/account/dashboard" className="btn btn-light mt-3">Home</Link>
       </div>
     </section>
   </section>
