2a78677e64f3c696fa189b3c1a249036e79a4f1b
Jefferson Vieira da Silva <jefferson98.vieira@gmail.com>
2019-05-25 15:03:10 -0300

    SGF-18 fix estilos

--- src/pages/Bank/New.jsx
+++ src/pages/Bank/New.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Card = ({ addAccount }) => (
-  <div className="card" title="Nova conta" onClick={addAccount}>
-    <div className="card-body card-add">
+  <div className="bank__add card" title="Nova conta" onClick={addAccount}>
+    <div className="card-body">
       <div className="card-add">
         <FontAwesomeIcon icon="plus" size="6x" />
       </div>
